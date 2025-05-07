099617599e0efd9a3b8d9bd13d38b558741dddd5
DamGarcia <damg883@gmail.com>
2025-05-06 21:37:41 -0400

    In class example for Test class

---
+++ CarDemo/src/test/java/com/pluralsight/CarTest.java
@@ -0,0 +1,55 @@
+package com.pluralsight;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+  
+    @Test
+    public void accelerate_should_increaseCarSeed(){
+        // arrange
+        Car c = new Car("Ford", "Mustang");
+        int speedChange = 15;
+        int expectedSpeed = 15;
+        
+        // acct
+        c.accelerate(speedChange);
+        
+        // assert
+        int actualSpeed = c.getSpeed();
+        assertEquals(expectedSpeed, actualSpeed);
+    }
+
+    @Test
+    public void brake_should_decreaseCarSpeed() {
+        // arrange
+        Car car = new Car("Ford", "F150 Raptor");
+        int speedUpBy = 15;
+        int slowDownBy = 10;
+        int expectedSpeed = 5;
+        car.accelerate(speedUpBy);
+        
+        // act
+        car.brake(slowDownBy);
+        
+        // assert
+        int actualSpeed = car.getSpeed();
+        assertEquals(expectedSpeed, actualSpeed);
+    }
+
+    @Test
+    public void brake_should_stopCar_whenChangeIsGreater() {
+        // arrange
+        Car car = new Car("Ford", "F150 Raptor");
+        int speedUpBy = 15;
+        int slowDownBy = 20;
+        int expectedSpeed = 0;
+        car.accelerate(speedUpBy);
+        // act
+        car.brake(slowDownBy);
+        // assert
+        int actualSpeed = car.getSpeed();
+        assertEquals(expectedSpeed, actualSpeed);
+    }
+}
